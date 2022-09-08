284332ef4f97305b8e645743fe87d9c83a023d23
Wehba Korouni <WEHKO@brkint.dk>
2022-09-08 08:28:41 +0200

    fetch scope data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,10 +10,10 @@ function Home( props) {
     <Body/>
     <div className="row">
      
-    <Card1 name="Scope" see={<a href = "/scope">scope</a>} description="data about scopes"/>
+    <Card1 name="Scope" see={<a  href = "/scope"><h2 style={{ color: 'grey' }}>scope</h2></a>} description="data about scopes"/>
 
-    <Card1 name="revsations" see={<a href='/revistion'>revstion</a>} description="data about revistion"/>
-    <Card1 name="leases" see={<a href='/leases'>Leases</a>} description="data about leases" />
+    <Card1 name="revsations" see={<a href='/revistion'><h2 style={{ color: 'grey' }}>revistion</h2></a>} description="data about revistion"/>
+    <Card1 name="leases" see={<a href='/leases'><h2 style={{ color: 'grey' }}>leases</h2></a>} description="data about leases" />
     </div>
     </div>
   );
--- src/components/Scopes.jsx
+++ src/components/Scopes.jsx
@@ -9,8 +9,7 @@ function Scopes() {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      'Origin': '',
-      'Host': 'api.producthunt.com',
+      'Host': 'central.brkint.dk',
       'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoid2Voa28iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9oYXNoIjoiZmVmMzA5NDYtZmZhYy00YmRlLThhMTktYWI5ZmQyMGJhNzBiIiwic3ViIjoiUG93ZXJTaGVsbFVuaXZlcnNhbCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJXZWJ0b29sc19Vc2VyIiwiUmVhZGVyIiwiT3BlcmF0b3IiXSwibmJmIjoxNjYwODEzNDIxLCJleHAiOjE2OTIzNDk0MjEsImlzcyI6Iklyb25tYW5Tb2Z0d2FyZSIsImF1ZCI6IlBvd2VyU2hlbGxVbml2ZXJzYWwifQ.e-wPmHX19HBQdmbEXesQ8JOZI56qKnhjzPw7VdxY0k8',
     }
   }
